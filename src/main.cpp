12cad6793d95234d3f43b049183ccdfdb0990df0
CelisPerez <a24310327@live.ceti.mx>
2025-12-09 21:24:45 -0600

    Cambios
    Todo cambio

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow window{ sf::VideoMode{800, 600}, "Ventana Negra - SFML 3.0.2" };
+
+    while (window.isOpen()) {
+        // No usamos pollEvent(), así no aparece std::optional
+        // La ventana solo puede cerrarse con ALT+F4 o el botón X (SO se encarga)
+        
+        window.clear(sf::Color::Black);
+        window.display();
+    }
+
+    return 0;
+}
